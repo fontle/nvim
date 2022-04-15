44fb59efe615518879c94222f57c8d01e8cb6f31
Isaac Beight-Welland <68354631+fontle@users.noreply.github.com>
2022-04-15 16:49:43 +0100

    Updated packer_init to include bootstrap
    Packer.nvim now automatically installs if it cannot be found on the machine.

--- lua/packer_init.lua
+++ lua/packer_init.lua
@@ -9,6 +9,11 @@
 --- neovim-lua/README.md
 --- https://github.com/brainfucksec/neovim-lua#readme
 
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
 
 local cmd = vim.cmd
 cmd [[packadd packer.nvim]]
